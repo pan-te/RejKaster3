1a0ed802b864a39c9a84cc0260a6ed5fc3b1e9cd
normaluser <tematee2@gmail.com>
2023-10-05 21:10:03 +0200

    many comments added...

--- misc.pas
+++ misc.pas
@@ -1,66 +1,68 @@
-function load_music : boolean;
-begin
-  if Mix_OpenAudio( MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096 ) < 0 then HALT;
-  music := Mix_LoadMUS('megadens.ogg');
-  if music = nil then HALT;
-  Mix_VolumeMusic( MIX_MAX_VOLUME );
-  if Mix_PlayMusic( music, -1 ) < 0 then begin Writeln('Playback failed!');
-  end else Writeln('Playback started.');
-  load_music:= true;
-end;
+function load_music() : boolean;
+         begin
+           if Mix_OpenAudio( MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096 ) < 0 then HALT;
+	   music := Mix_LoadMUS('megadens.ogg');
+           if music = nil then HALT;
+           Mix_VolumeMusic( MIX_MAX_VOLUME );
+           if Mix_PlayMusic( music, -1 ) < 0 then begin Writeln('Playback failed!');
+           end else Writeln('Playback started.');
+           load_music:= true;
+   end;
 
-function distray_x(ray_l,alpha:real):real;
-begin
-  distray_x:=ray_l*cos(alpha*degtorad);
-end;
+function distray_x(ray_l,alpha:real): real;
+         begin
+         distray_x:=ray_l*cos(alpha*degtorad);
+         end;
 
-function distray_y(ray_l,alpha:real):real;
-begin
-  distray_y:=ray_l*sin(alpha*degtorad);
-end;
-
-procedure SetPlayer(x,y:integer);
-begin
-  pl_x:=map_scale*x;                         // player positioned in middle of the square !
-  pl_y:=map_scale*y;                         // without substract (!) half of the square !! (map_scale DIV 2 = 8)
-end;
+function distray_y(ray_l,alpha:real): real;
+         begin
+         distray_y:=ray_l*sin(alpha*degtorad);
+         end;
+	
+	 procedure SetPlayer(x,y:integer);
+	 begin
+		  pl_x:=map_scale*x;       // player positioned in middle of the square !
+		  pl_y:=map_scale*y;       // without substract (!) half of the square !! (map_scale DIV 2 = 8)
+	 end;
 
 procedure load_map(mapname:string);
-var i,j,k,l:integer;
-    mapfile:file of smallint;
-begin
-  Assign(mapfile,mapname);
-  Reset(mapfile);
-  for i:=0 to max_Anz do
-    for j:=0 to max_Anz do
-      read(mapfile,map[i,j]);
-  Close(mapfile);
-
-  for k:=0 to max_Anz do
-    for l:=0 to max_Anz do
-    begin
-      if map[k,l]=9 then
-      begin
-        SetPlayer(k,l);
-        map[k,l]:=0;
-        writeln('Player placed at X: ', k, ' Y: ', l);
-        break;
-      end;
-    end;
-  Writeln('Map loaded successfully!');
-end;
-
-procedure GameInit;
-begin
-  if SDL_Init(SDL_INIT_VIDEO OR SDL_INIT_AUDIO )<0 then HALT
-                                                   else writeln('Initializing SDL... ok.');
-  window := SDL_CreateWindow('Raycaster4',SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,scr_width,scr_height,SDL_WINDOW_SHOWN);
-  rend := SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
-  SDL_ShowCursor(0);                                                         //don't show (mouse) cursor in the window
-  SDL_SetRenderDrawColor(rend, 0, 0, 0, 0);                                  //black color
-  SDL_RenderClear(rend);
-  SDL_RenderPresent(rend);                                                   //creating and cleaning the window
-  SDL_WarpMouseInWindow(window,mouswidth,mousheight);                        //initializing MousePos
-  rotate :=0.0;                                                              //angle of playerview horizontal
-  rotateZ:=0;                                                                //angle of playerview vertical
-end;
+	 var i,j, k,l: integer;
+		mapfile: file of smallint;
+	 begin
+		Assign(mapfile, mapname);
+		 Reset(mapfile);
+		 for i:=0 to max_Anz do begin
+			 for j:=0 to max_Anz do begin
+				 read(mapfile, map[i,j]);
+			 end;
+		 end;
+		 Close(mapfile);
+		 for k:=0 to max_Anz do begin
+			 for l:=0 to max_Anz do begin
+				if map[k,l]=9 then begin
+					SetPlayer(k,l);
+					map[k,l]:=0;
+					writeln('Player placed at X: ', k, ' Y: ', l);
+					break;
+			 end;
+		end;
+	end;
+			 Writeln('Map loaded successfully!');
+	end;
+	
+procedure GameInit();
+	 begin
+		   if SDL_Init( SDL_INIT_VIDEO or SDL_INIT_AUDIO ) < 0 then begin HALT;
+		  end else writeln ('Initializing SDL... ok.');
+		   window:= SDL_CreateWindow('RejKaster4',64,64,scr_width,scr_height,SDL_WINDOW_SHOWN);
+				  //SDL_CreateWindow('RejKaster4',SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,scr_width,scr_height,SDL_WINDOW_SHOWN);
+		  //new(event);                                               //creating an event
+		  rend:= SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+		  SDL_ShowCursor(0);
+		  SDL_SetRenderDrawColor(rend, 0,0,0,0);
+		  SDL_RenderClear(rend);
+		  SDL_RenderPresent(rend);                                   //creating and cleaning the window
+		  SDL_WarpMouseInWindow(window, mouswidth, mousheight);      //initialize MousePos
+		  rotate:=0;                                                 //angle of playerview horizontal
+		  rotatez:=0;                                                //angle of playerview vertical
+	end;
--- rejkaster.pas
+++ rejkaster.pas
@@ -11,57 +11,67 @@
 
 
 program rejkaster;
+{$mode objFPC}                                  //object pascal extension on; [ or use $mode FPC or $mode DELPHI ]
 uses crt, sdl2, SDL2_mixer, SDL2_image;
-{$ASMMODE intel}                            //assembly syntax setting, unnecessary
+
+const
+  FOV        = 80;                               //Field of View: 80 degree
+  halfFOV    = 40;                               //degree
+  FOVWinX    = 400;                              //Field of View: 400 pixel; x direction
+  FOVWinY    = 300;                              //Field of View: 300 pixel; y direction
+  ray_dL     = 0.2;                              //const to increase the length of the ray
+  ray_dG     = 0.2;                              //const to increase the angle of the ray in degree
+  degtorad   = 0.01745;                          //(Pi / 180 Grad) = (RAD to Grad)
+  scr_width  = 1200;                             //Screen width
+  scr_height = 900;                              //Screen height
+  halfheight = 450;
+  mouswidth  = 400;                              //theoretical half of screenwidth = 600;  will be mousePos startpoint in game
+  mousheight = 300;                              //theoretical half of screenheight= 450;
+  map_scale  = 16;                               //can also be larger: [ 32 or 64 ]: size of wall block (square of the grid)
+  draw_dist  = 191;                              //draw_dist := map_scale * 5 - 1; [ 191 := 16 * 5 -1 ] how far you can see in pixel
+  max_Anz    = 15;                               //width and height of the (square-) map
+  player_mov = 3;                                //player moves 3 pixels
+  levelname  = 'test.map';                       //map name
+
 var
-  pl_x, pl_y: real;                      //view position(coordinates swapped!)
-  rotate : real;                            //rotation angle
-  rotatez : integer;
-  loop: integer;
-  loop1: real;                                          //variables to the loop (loop is the relative angle of incidence of the ray)
-  ray_deg: Real;                            //absolute angle of incidence of the ray
-  ray_dist: integer;                        //distance traveled by the ray
-  ray_realdist: real;
-  draw_yoffset_up, draw_yoffset_down: integer;                    //distance of the beginning of the vertical line from the x-axis of the screen
-  draw_yoffset2: real;
-  ray_draw: Real;
-  color_draw: integer;
-  dist_x,dist_y:real;                       //radius vector coordinates
-  block_posx,block_posy:integer;            //the point of incidence of the ray on the map
-  event: tSDL_event;                        //keyboard event now not a pointertype
-  rend: pSDL_renderer;                      //SDL-specific variables
-  window:  pSDL_window;
-  music: PMIX_music;
-  Keycodes:  ^byte;                         //keyboard status
-  background: pSDL_Texture;
-  mouse_x: integer;                         // Cursor position
-  mouse_y: integer;
-  movebob_up, movebob_down: real;
-  bob_phase: boolean;
-  map:array[0..15,0..15] of smallint;
-  levelname: string;
-  //i: integer;
-
-const                                       //Constant
-//block_size=8;
-//map_size=128;
-fov=80;                                     //degree angle;  5*80 = 400 pixel = fov in pixel  (400x300)
-halffov=40;                                 //degree angle
-degtorad=0.01745;
-scr_width=1200;                             //size of window  (1200x900)
-scr_height=900;                             //size of window
-halfheight=450;
-draw_dist=191;
-map_scale=16;                               //size of wall block (square of the grid)
-
+  pl_x,
+  pl_y              : real;                      //view position(coordinates swapped!)
+  rotate            : real;                      //horizontal rotation angle  left / right
+  rotateZ           : integer;                   //vertical rotation angle    up   / down
+  loop              : integer;
+  loop1             : real;                      //variables to the loop (loop is the relative angle of incidence of the ray)
+  ray_deg           : real;                      //absolute angle of incidence of the ray
+  ray_dist          : integer;                   //loop variable like i or j
+  ray_realdist      : real;                      //length of the searching ray in pixel, will be increased during the main loop
+  draw_yoffset_up,
+  draw_yoffset_down : integer;                   //distance of the beginning of the vertical line from the x-axis of the screen
+  draw_yoffset2     : real;
+  ray_draw          : real;
+  color_draw        : integer;
+  dist_x,
+  dist_y            : real;                      //radius vector coordinates
+  block_posx,
+  block_posy        : integer;                   //the point of incidence of the ray on the map
+  event             : TSDL_event;                //keyboard event
+  rend              : PSDL_renderer;             //SDL2-specific variables
+  window            : PSDL_window;
+  music             : PMIX_music;
+  Keycodes          : ^byte;                     //keyboard status
+  background        : PSDL_Texture;
+  mouse_x           : integer;                   //mousecursor position
+  mouse_y           : integer;
+  movebob_up,
+  movebob_down      : real;
+  bob_phase         : boolean;
+  map               : array[0..max_Anz, 0..max_Anz] of smallint;
 
 {$I movement.pas}
 {$I rendering.pas}
 
 begin
   clrscr;
-  Write('Enter level filename: ');
-  Readln(levelname);
+  //Write('Enter level filename: ');
+  //Readln(levelname);
   load_map(levelname);
   GameInit();
   load_music();
@@ -80,30 +90,32 @@ begin
   Writeln('==================Enjoy====================');
   Writeln();
 
-    repeat                                                          //beginning of the main loop
-     Controls();
-     DrawBackground(background, rotatez);
-     loop1 := 0;
-    for loop:=0 to (5*fov) do                                          //casting a ray
+  repeat                                                                     //beginning of the main loop
+    Controls;                                                                //read keyboard
+    DrawBackground(background, rotateZ);
+    loop1 := 0.0;
+    for loop := 0 to FOVWinX do                                              //casting a ray
     begin
-        ray_deg:=rotate+halffov-loop1;
-        ray_realdist:=0;
-        for ray_dist:=1 to 5*draw_dist do
-        begin
-           dist_x:= distray_x(ray_realdist,ray_deg);
-           dist_y:= distray_y(ray_realdist,ray_deg);
-           block_posx:=round((pl_x +dist_x)/map_scale);
-           block_posy:=round((pl_y +dist_y)/map_scale);
-           if (block_posx < 0) or (block_posx > 15) then block_posx:=1;
-           if (block_posy < 0) or (block_posy > 15) then block_posy:=1;  //kills the error of detecting a non-existent block //calculating the final coordinates of the ray
-           if (map[block_posx, block_posy]>=1) then draw_line(ray_realdist, block_posx, block_posy, loop1,rotatez*3);
-           ray_realdist:=ray_realdist+0.2;
-           if map[block_posx,block_posy]>=1 then break;                    //ends the loop when a wall is detected
-        end;
-        loop1 := loop1 + 0.2;
-     end;
-     SDL_RenderPresent(rend);
-     SDL_Delay(17);
-  until false;
+      ray_deg := rotate + halfFOV - loop1;                                   //ray angle in degree
+      ray_realdist := 0;
+      for ray_dist := 1 to (5 * draw_dist) do                                //5*draw_dist: max length of the ray without hit a wall
+      begin
+        dist_x := distray_x(ray_realdist, ray_deg);                          //search for a wall in x; increase the raylength [ray_realdist] with
+        dist_y := distray_y(ray_realdist, ray_deg);                          //a small amount [ here 0.2 pixel ] at the end of the loop
+        block_posx := round((pl_x + dist_x) / map_scale);                    //control: if the ray hits a wall...
+        block_posy := round((pl_y + dist_y) / map_scale);
+
+        if (block_posx < 0) OR (block_posx > max_Anz) then block_posx := 1;  //kills the error of detecting a non-existent block
+        if (block_posy < 0) OR (block_posy > max_Anz) then block_posy := 1;  //calculating the final coordinates of the ray
+
+        if (map[block_posx, block_posy] >= 1) then draw_line(ray_realdist, block_posx, block_posy, loop1, rotateZ * 3);
+        ray_realdist := ray_realdist + ray_dL;                               //increase the length of the searching ray [ ray_realdist ]
+        if map[block_posx, block_posy] >= 1 then break;                      //ends the loop when a wall is detected
+      end;
+      loop1 := loop1 + ray_dG;                                               //change the angle of the ray [ here 0.2 degree ]
+    end;
+    SDL_RenderPresent(rend);                                                 //update the screen with any rendering performed since the previous call
+    SDL_Delay(17);                                                           //delay 17 miliseconds equal to 60 FPS = Frames per second
+  until false;                                                               //end of main loop
 end.
 
