3b8129946a9fc11e86934cb9903a1dd5b12b82ca
normaluser <tematee2@gmail.com>
2023-10-05 20:19:27 +0200

    many comments added

--- maptester.pas
+++ maptester.pas
@@ -1,59 +1,59 @@
 program tester;
-uses sysutils;
+uses crt, sysutils;
+
+const
+max=15;    //"wall cubes" or "squares" of the map
+map:array[0..max,0..max] of smallint=((1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
+                                      (1,2,1,0,0,0,0,0,0,0,0,0,0,0,0,1),
+                                      (1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
+                                      (1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
+                                      (1,0,0,0,2,2,2,2,2,2,2,2,0,0,0,1),
+                                      (1,0,0,0,2,0,0,0,0,0,0,2,0,0,0,1),
+                                      (1,0,0,0,2,0,2,2,2,2,0,2,0,0,0,1),
+                                      (1,0,0,0,2,0,2,3,9,2,0,2,0,0,0,1),
+                                      (1,0,0,0,2,0,2,2,0,2,0,2,0,0,0,1),
+                                      (1,0,0,0,2,0,2,2,0,2,0,2,0,0,0,1),
+                                      (1,0,0,0,2,0,0,0,0,2,0,2,0,0,0,1),
+                                      (1,0,0,0,2,2,2,2,2,2,0,2,0,0,0,1),
+                                      (1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
+                                      (1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
+                                      (1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
+                                      (1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
 
 var
-	k,l: integer;
-	name: string;
-	
-	
-const
-map:array[0..15,0..15] of integer =((1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
-							(1,2,1,0,0,0,0,0,0,0,0,0,0,0,0,1),
-							(1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-							(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-							(1,0,0,0,2,2,2,2,2,2,2,2,0,0,0,1),
-							(1,0,0,0,2,0,0,0,0,0,0,2,0,0,0,1),
-							(1,0,0,0,2,0,2,2,2,2,0,2,0,0,0,1),
-							(1,0,0,0,2,0,2,3,9,2,0,2,0,0,0,1),
-							(1,0,0,0,2,0,2,2,0,2,0,2,0,0,0,1),
-							(1,0,0,0,2,0,2,2,0,2,0,2,0,0,0,1),
-							(1,0,0,0,2,0,0,0,0,2,0,2,0,0,0,1),
-							(1,0,0,0,2,2,2,2,2,2,0,2,0,0,0,1),
-							(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-							(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-							(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1),
-							(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
-							
+k,l: integer;
+name: string;
+
 procedure generate_map();
-	 var i,j: integer;
-		mapfile: file of smallint;      //not integer
-	 begin
-		 Assign(mapfile, name);
-		 rewrite(mapfile);
-		 for i:=0 to 15 do begin
-			 for j:=0 to 15 do begin
-				 write(mapfile, map[i,j]);
-			 end;
-		 end;
-		
-		 {for i:=0 to 15 do begin	
-			write(mapfile, map[i,0],map[i,1],map[i,2],map[i,3],map[i,4],map[i,5],map[i,6],map[i,7],map[i,8],map[i,9],map[i,10],map[i,11],map[i,12],map[i,13],map[i,14],map[i,15]);
-			//map[i,j]:=StrToInt(table[i,j]);
-		 end;}
-			Close(mapfile);
-	 end;
-	
-	 begin
-		 write('Enter a filename: ');
-		 readln(name);
-		 writeln;
-		 generate_map();
-			for k := 0 to 15 do begin      // show ascii map on screen
-			  for l := 0 to 15 do begin
-				if map[k,l] = 0 then write('  ')
-								else write(map[k,l], ' ');
-				end;
-			    writeln;
-			end;
-	writeln;
+var i,j : integer;
+    mapfile : file of smallint;
+begin
+  Assign(mapfile, name);
+  rewrite(mapfile);
+  for i := 0 to max do begin
+    for j := 0 to max do begin
+      write(mapfile, map[i,j]);
+    end;
+  end;
+
+ {for i:=0 to max do begin
+  write(mapfile, map[i,0],map[i,1],map[i,2],map[i,3],map[i,4],map[i,5],map[i,6],map[i,7],map[i,8],map[i,9],map[i,10],map[i,11],map[i,12],map[i,13],map[i,14],map[i,15]);
+  //map[i,j]:=StrToInt(table[i,j]);
+  end;}
+  Close(mapfile);
+end;
+
+begin
+  clrscr;
+  write('Enter a filename: ');
+  readln(name); writeln;
+  generate_map();
+  for k := 0 to max do begin      // show ascii map on screen
+    for l := 0 to max do begin
+      if map[k,l] = 0 then write('  ')
+                      else write(map[k,l], ' ');
+    end;
+    writeln;
+  end;
+  writeln;
 end.
--- rendering.pas
+++ rendering.pas
@@ -1,30 +1,30 @@
-procedure draw_line(dist: Real; posx, posy: integer; angle: Real; anglez: integer);
-           var color_draw2: integer;
-	   begin
-           ray_draw:= dist*cos((angle - halffov)*degtorad);
-	   color_draw:=trunc(dist)+1;
-	   color_draw2:=trunc(color_draw*2/3);
-	   case map[posx,posy] of
-            1: SDL_SetRenderDrawColor(rend,192-color_draw,128-color_draw2,0,0);
-	    2: SDL_SetRenderDrawColor(rend,0,128-color_draw2,192-color_draw,0);
-	    3: SDL_SetRenderDrawColor(rend,129-color_draw2,128-color_draw2,192-color_draw,0);
-	   end;
-           draw_yoffset2:= (trunc(12*halfheight/(ray_draw+1)));
-           draw_yoffset_up:=trunc(draw_yoffset2*movebob_down);
-           draw_yoffset_down:=trunc(draw_yoffset2*movebob_up);
-           SDL_RenderDrawLine(rend, 3*loop, halfheight-draw_yoffset_up+anglez,3*loop, halfheight+draw_yoffset_down+anglez);	//rysuję linię
-           SDL_RenderDrawLine(rend, 3*loop+1, halfheight-draw_yoffset_up+anglez,3*loop+1, halfheight+draw_yoffset_down+anglez);	//rysuję linię
-           SDL_RenderDrawLine(rend, 3*loop+2, halfheight-draw_yoffset_up+anglez,3*loop+2, halfheight+draw_yoffset_down+anglez);	//rysuję linię
-           end;
+procedure draw_line(dist : Real; posx, posy : integer; angle : Real; anglez : integer);
 
-procedure DrawBackground(bgimg: pSDL_Texture;zangle: integer) ;
-         var draw_gnd_rect: tSDL_Rect;                            //not a pointertype now
-         begin
-               //new(draw_gnd_rect);                              //memory leak! NEW without DISPOSE!
-               draw_gnd_rect.x:=0;
-               draw_gnd_rect.y:=zangle*3-269;
-               draw_gnd_rect.w:=scr_width;
-               draw_gnd_rect.h:=scr_height+540;
-               SDL_RenderCopy(rend,bgimg,nil,@draw_gnd_rect);
-         end;
+var color_draw2 : integer;
 
+begin
+  ray_draw := dist * cos((angle - halffov) * degtorad);
+  color_draw := trunc(dist) + 1;
+  color_draw2 := trunc(color_draw * 2/3);
+  case map[posx,posy] of
+    1: SDL_SetRenderDrawColor(rend, 192 - color_draw, 128 - color_draw2, 0,                0);
+    2: SDL_SetRenderDrawColor(rend, 0,                128 - color_draw2, 192 - color_draw, 0);
+    3: SDL_SetRenderDrawColor(rend, 129 -color_draw2, 128 - color_draw2, 192 - color_draw, 0);
+  end;
+  draw_yoffset2 := (trunc(12 * halfheight / (ray_draw + 1)));
+  draw_yoffset_up :=   trunc(draw_yoffset2 * movebob_down);
+  draw_yoffset_down := trunc(draw_yoffset2 * movebob_up);
+  SDL_RenderDrawLine(rend, 3 * loop,     halfheight - draw_yoffset_up + anglez, 3 * loop,     halfheight + draw_yoffset_down + anglez);	//draw a linie
+  SDL_RenderDrawLine(rend, 3 * loop + 1, halfheight - draw_yoffset_up + anglez, 3 * loop + 1, halfheight + draw_yoffset_down + anglez);	//draw a linie
+  SDL_RenderDrawLine(rend, 3 * loop + 2, halfheight - draw_yoffset_up + anglez, 3 * loop + 2, halfheight + draw_yoffset_down + anglez);	//draw a linie
+end;
+
+procedure DrawBackground(bgimg : pSDL_Texture; zangle : integer) ;
+var draw_gnd_rect : tSDL_Rect;
+begin
+  draw_gnd_rect.x := 0;
+  draw_gnd_rect.y := zangle * 3 - 269;
+  draw_gnd_rect.w := scr_width;
+  draw_gnd_rect.h := scr_height + 540;
+  SDL_RenderCopy(rend, bgimg, nil, @draw_gnd_rect);
+end;
